91a3fecce75493fdeed0f7d42023294cafe72172
disaruina <maksimskurskij@gmail.com>
2026-09-14 13:09:37 +0300

    Add Todo class

--- bin/lab2.dart
+++ bin/lab2.dart
@@ -1,3 +1,12 @@
-void main() {
-  
+import 'package:lab2/todo.dart';
+
+void main(){
+  Todo task1 = Todo(
+    id: 1,
+    title: 'Купить продукты',
+  );
+  Todo task2 = Todo(id: 2, title: 'Сделать зарядку');
+  task1.complete();
+  print(task1);
+  print(task2);
 }
---
+++ lib/todo.dart
@@ -0,0 +1,24 @@
+class Todo {
+  int id;
+  String title;
+  bool isDone;
+
+
+  // Todo(int id, String title) {
+  //   this.id = id;
+  //   this.title = title;
+  //   this.id = false;
+  // }
+  //int? id;
+  //String title = '';
+  //bool isDone = false;
+  Todo({required this.id, required this.title}): isDone = false;
+  @override
+  String toString() {
+    String status = isDone ? '[x]' : '[ ]';
+  return '$status $id. $title';
+  }
+  void complete() {
+    isDone = true;
+  }
+}
