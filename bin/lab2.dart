aefba9ffe3cf50b5fa90d72b471387b81cedb4ac
disaruina <maksimskurskij@gmail.com>
2026-09-23 21:11:04 +0300

    Add ToDo CLI app

--- bin/lab2.dart
+++ bin/lab2.dart
@@ -1,23 +1,14 @@
-// void main(){
-//   Todo task1 = Todo(
-//     id: 1,
-//     title: 'Купить продукты',
-//   );
-//   Todo task2 = Todo(id: 2, title: 'Сделать зарядку');
-//   task1.complete();
-//   print(task1);
-//   print(task2);
-// }
 import 'dart:io';
 import 'package:lab2/todo.dart';
+
 void printMenu() {
   print('');
   print('ToDo список');
-  print('add   - добавить задачу');
-  print('list  - показать все задачи');
-  print('done  - отметить выполненной');
-  print('dalete - удалить задачу');
-  print('exit  - выйти');
+  print('add    - добавить задачу');
+  print('list   - показать все задачи');
+  print('done   - отметить выполненной');
+  print('delete - удалить задачу');
+  print('exit   - выйти');
 }
 
 void addTodo(List<Todo> todos) {
@@ -52,8 +43,64 @@ void completeTodo(List<Todo> todos) {
 
   if (input == null) return;
 
-  int? id == int.tryParse(input.trim());
-  if (id == null) return;
+  int? id = int.tryParse(input.trim()); 
+  
+  if (id == null) { 
     print('Ошибка: введите число');
     return;
+  }
+  
+  for (var todo in todos) {
+    if (todo.id == id) {
+      todo.complete();
+      print('Задача отмечена выполненной!');
+      return;
+    }
+  }
+
+  print('Задача с ID $id не найдена');
+}
+
+void deleteTodo(List<Todo> todos) {
+  stdout.write('ID задачи: ');
+  String? input = stdin.readLineSync();
+
+  if (input == null) return;
+
+  int? id = int.tryParse(input.trim());
+  if (id == null) {
+    print('Ошибка: введите число');
+    return;
+  }
+
+  for (int i = 0; i < todos.length; i++) {
+    if (todos[i].id == id) {
+      todos.removeAt(i);
+      print('Задача удалена!');
+      return;
+    }
+  }
+
+  print('Задача с ID $id не найдена');
+}
+
+void main() {
+  List<Todo> todos = [];
+  printMenu();
+
+  while (true) {
+    stdout.write('> ');
+    String? input = stdin.readLineSync();
+    if (input == null) continue;
+    String command = input.trim().toLowerCase();
+    if (command.isEmpty) continue;
+    switch (command) {
+      case 'add': addTodo(todos); break;
+      case 'list': listTodos(todos); break;
+      case 'done': completeTodo(todos); break;
+      case 'delete': deleteTodo(todos); break;
+      case 'exit': print('До свидания!'); return;
+      default: printMenu(); print('Неизвестная команда.');
+    }
+  }
 }
